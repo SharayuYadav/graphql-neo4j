e4b5792375278f96bd96f01286b7a88487861594
sharayu_y <sharayu_y@techjoomla.com>
2023-08-25 14:34:23 +0530

    Graphql Project added to git

--- index.js
+++ index.js
@@ -2,28 +2,54 @@ const { gql, ApolloServer } = require("apollo-server");
 const { Neo4jGraphQL } = require("@neo4j/graphql");
 const neo4j = require("neo4j-driver");
 require("dotenv").config();
+const fs = require('fs'); 
+let dataString = '';
+let dataStr = '';
 
-const typeDefs = gql`
-  type Masterlists {
-    name: String!
-    id:Int!
-    alias: String!
-  }
-`;
 
-const driver = neo4j.driver(
-  process.env.NEO4J_URI,
-  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
-);
-
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
-
-neoSchema.getSchema().then((schema) => {
-    const server = new ApolloServer({
-        schema: schema
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
     });
+  });
+}
 
-    server.listen().then(({ url }) => {
-        console.log(`GraphQL server ready on ${url}`);
-    });
+readFiles('types_renamed/', function(filename, content) {
+  dataStr += content + '\n';
+}, function(err) {
+  throw err;
 });
+
+setTimeout(() => {
+  if (dataStr) {
+    const typeDefs = gql`${dataStr}`;
+  
+    const driver = neo4j.driver(
+      process.env.NEO4J_URI,
+      neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+    );
+    
+    const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+    
+    neoSchema.getSchema().then((schema) => {
+        const server = new ApolloServer({
+            schema: schema
+        });
+    
+        server.listen().then(({ url }) => {
+            console.log(`GraphQL server ready on ${url}`);
+        });
+    });
+  }
+}, 5000)
